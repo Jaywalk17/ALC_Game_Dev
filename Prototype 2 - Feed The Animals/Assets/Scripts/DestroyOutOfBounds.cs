a40f7baf354c32bc858c85a3ac8a9453fe832ded
Jonathan Walker <jonathan.walker2021@gmail.com>
2020-10-08 09:08:50 -0600

    Spawn Manager created

--- Prototype 2 - Feed The Animals/Assets/Scripts/DestroyOutOfBounds.cs
+++ Prototype 2 - Feed The Animals/Assets/Scripts/DestroyOutOfBounds.cs
@@ -4,17 +4,15 @@ using UnityEngine;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
     private float topBound = -15;
+    private float lowerBound = 35;
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.z > topBound){
+        if (transform.position.z < topBound){
+            Destroy(gameObject);
+        } else if (transform.position.z > lowerBound){
             Destroy(gameObject);
         }
 
--- Prototype 2 - Feed The Animals/Assets/Scripts/MoveForward.cs
+++ Prototype 2 - Feed The Animals/Assets/Scripts/MoveForward.cs
@@ -3,18 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveForward : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-    
+{    
     public float speed = 40;
 
     // Update is called once per frame
     void Update()
     {
+        // Moves projectile forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
