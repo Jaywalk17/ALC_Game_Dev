d477986b048da73e7238a50e8e9bd2df12816193
Jonathan Walker <jonathan.walker2021@gmail.com>
2020-11-06 09:05:53 -0700

    Background scrolling initiated

--- Prototype 3 - Run and Jump/Assets/Scripts/SpawnManager.cs
+++ Prototype 3 - Run and Jump/Assets/Scripts/SpawnManager.cs
@@ -6,16 +6,25 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
+
     private float startDelay = 2;
     private float repeatDelay = 2;
+
+    private PlayerController playerControllerScript;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnObstacle", startDelay, repeatDelay);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
+
     void SpawnObstacle(){
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        //Stop obstacle spawning on gameOver
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
     }
 
     // Update is called once per frame
