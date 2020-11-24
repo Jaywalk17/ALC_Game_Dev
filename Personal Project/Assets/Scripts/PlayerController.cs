aac96edd37e9b6cafa3f25aabe0d7268b158e8d9
Jonathan Walker <jonathan.walker2021@gmail.com>
2020-11-24 09:06:39 -0700

    Player Controller script created for Personal Project

---
+++ Personal Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+// Movement will be unlike that in other projects
+// Only one entity will move at a time, and only in one direction
+// Goes from space to space until an enemy is reached
