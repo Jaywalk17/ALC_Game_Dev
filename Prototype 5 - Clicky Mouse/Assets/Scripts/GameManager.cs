7a9414f50e2742af7573be79fd53e8edf0d781fe
Jonathan Walker <jonathan.walker2021@gmail.com>
2021-01-12 09:07:47 -0700

    Game over screen works, working on title screen

--- Prototype 5 - Clicky Mouse/Assets/Scripts/GameManager.cs
+++ Prototype 5 - Clicky Mouse/Assets/Scripts/GameManager.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,9 +11,11 @@ public class GameManager : MonoBehaviour
     private int score;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
-
+    public bool isGameActive;
     public List<GameObject> targets;
     private float spawnRate = 1.0f;
+
+    public Button restartButton;
 
 
     // Start is called before the first frame update
@@ -20,23 +24,31 @@ public class GameManager : MonoBehaviour
         StartCoroutine(SpawnTarget());
         score = 0;
         UpdateScore(0);
+        isGameActive = true;
     }
 
-    IEnumerator SpawnTarget(){
-        while(true){
+    IEnumerator SpawnTarget()
+    {
+        while(isGameActive){
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
         }
     }
 
-    public void GameOver(){
-        gameOverText.gameObject.SetActive(true);        
-    }
-
     public void UpdateScore(int scoreToAdd){
         score += scoreToAdd;
         scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver(){
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;        
+    }
+
+    public void RestartGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
 
